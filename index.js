82e22cf255a1c0c79660b85a3e8ffbccb9f4bf97
Huiying0108 <whymissu1228@gmail.com>
2018-12-17 13:02:03 -0800

    added letter.js word.js and index.js

---
+++ index.js
@@ -0,0 +1,72 @@
+
+var wordclass = require("./word.js");
+var inquirer = require("inquirer");
+var guesses_left = 10;
+var game_over = false;
+var letters_guessed = new Set();
+
+
+var word = new wordclass();
+
+var letters_left = word.letters.length;
+
+
+function ask(){
+    word.showLetters();
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "letter",
+            message: "Guess a letter"
+        }
+    ]).then( function(response){
+        // console.log(JSON.stringify(response,null,2));
+        guess(response.letter);
+        if(!game_over){
+            ask();
+        }
+    })
+}
+
+ask();
+
+//Logic for responding to guesses
+function guess(letter){
+    if(letter.length === 1){
+        if(!letters_guessed.has(letter)){
+            letters_guessed.add(letter);
+        }
+        else{
+            console.log("Letter has already been guessed!\n");
+            return;
+        }
+        var num_correct = word.checkGuess(letter);
+        console.log(num_correct);
+        if( num_correct > 0){
+            console.log("Correct!\n");
+            letters_left -= num_correct;
+        }
+        else{
+            guesses_left--;
+            console.log("Incorrect!  " + guesses_left + " guesses remaining\n");
+        }
+    
+        if(letters_left === 0){
+            console.log("You win!");
+            word.showLetters();
+            game_over = true;
+        }
+    
+        if(guesses_left === 0){
+            console.log("You lose!");
+            console.log("Word was " + word.word)
+            game_over = true;
+        }
+
+    }
+    else{
+        console.log("You can only guess one letter at a time!\n");
+    }
+    
+    
+}
---
+++ word.js
@@ -0,0 +1,53 @@
+var letter = require("./letter");
+var rand_word = require("random-words");
+
+function Word(){
+
+    //Pick a word and make the letters for it
+    var letters = [];
+
+    var possible_words = [];
+
+    //Get a random word
+    var word = rand_word();
+    this.word = word;
+    var split_word = word.split("");
+    // console.log(split_word);
+
+    //Make a Letter instance for each letter
+    split_word.forEach( function(l){
+        var temp = new letter(l);
+        letters.push(temp);
+    });
+
+    this.letters = letters;
+
+
+    //Display word with letters shown or hidden
+    this.showLetters = function(){
+        var display = "";
+        this.letters.forEach( function(letter){
+            display += letter.getChar() + " ";
+        });
+
+        // display = display.slice(0, -1);
+
+        console.log(display);
+    }
+
+    this.checkGuess = function( guess ){
+        var matches_found = 0;
+        this.letters.forEach( function(letter){
+            if( letter.guessed === false && letter.checkGuess(guess) === true){
+                letter.guessed = true;
+                matches_found++;
+            }
+        });
+
+        return matches_found;
+        
+    }
+}
+// Word();
+
+module.exports = Word;
